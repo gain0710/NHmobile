98b0fac03a558f3957a1d659e1e8fc28d6e80c72
gain <gain0710@gmail.com>
2026-01-20 16:43:30 +0900

    Refine UI: remove wrapper bg and optimize status bar image scaling

--- App.tsx
+++ App.tsx
@@ -107,7 +107,7 @@ const App: React.FC = () => {
 
             {/* Status Bar */}
             <div className="h-12 bg-[#046fd9] flex-shrink-0 px-5">
-              <img src={statusBarImg} alt="Status Bar" className="w-full h-full object-cover" />
+              <img src={statusBarImg} alt="Status Bar" className="w-full h-full object-contain" />
             </div>
 
             {/* Scrollable Content */}
